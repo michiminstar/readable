eb75ab46cf9276bd18faf706f69c00514ab6eb90
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-07 20:57:34 +0900

    Add tabs for filtering by categories

---
+++ app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import CategoryTab from './CategoryTabs'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <CategoryTab />
+      </div>
+    )
+  }
+}
+
+export default Home
