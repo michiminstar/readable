3244fff4c7c2632e18c7c209241a76e4fdcab4cf
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-14 15:41:26 +0900

    Change the file name

--- app/src/actions/constants.js
+++ app/src/actions/constants.js
@@ -1,4 +1,4 @@
-export const GET_CATEGORY = 'GET_CATEGORY'
+export const GET_CATEGORIES = 'GET_CATEGORIES'
 
 export const GET_POSTS = 'GET_POSTS'
 export const GET_POSTS_BY_CATEGORY = 'GET_POSTS_BY_CATEGORY'
