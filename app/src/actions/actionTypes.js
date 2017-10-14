82cbfc7c09e2ce7cda96dbd1dcb7d0e82aa61f8a
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-14 14:55:09 +0900

    Add actions types

---
+++ app/src/actions/actionTypes.js
@@ -0,0 +1,15 @@
+export const GET_CATEGORY = 'GET_CATEGORY'
+
+export const GET_POSTS = 'GET_POSTS'
+export const GET_POSTS_BY_CATEGORY = 'GET_POSTS_BY_CATEGORY'
+export const ADD_POST = 'ADD_POST'
+export const EDIT_POST = 'EDIT_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const VOTE_POST = 'VOTE_POST'
+export const SORT_POST = 'SORT_POST'
+
+export const GET_COMMENTS = 'GET_COMMENTS'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const VOTE_COMMENT = 'VOTE_COMMENT'
