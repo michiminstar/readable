da58bebcb839b8501677daea4fbb28107af5d305
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-15 00:47:08 +0900

    Rename action type names

--- app/src/actions/constants.js
+++ app/src/actions/constants.js
@@ -1,14 +1,16 @@
-export const GET_CATEGORIES = 'GET_CATEGORIES'
+// Actions - JavaScript objects that describe any event that should update the // application’s state
 
-export const GET_POSTS = 'GET_POSTS'
-export const GET_POSTS_BY_CATEGORY = 'GET_POSTS_BY_CATEGORY'
+export const LOAD_CATEGORIES = 'LOAD_CATEGORIES'
+
+export const LOAD_POSTS = 'LOAD_POSTS'
+export const LOAD_POSTS_BY_CATEGORY = 'LOAD_POSTS_BY_CATEGORY'
 export const ADD_POST = 'ADD_POST'
 export const EDIT_POST = 'EDIT_POST'
 export const DELETE_POST = 'DELETE_POST'
 export const VOTE_POST = 'VOTE_POST'
 export const SORT_POST = 'SORT_POST'
 
-export const GET_COMMENTS = 'GET_COMMENTS'
+export const LOAD_COMMENTS = 'LOAD_COMMENTS'
 export const ADD_COMMENT = 'ADD_COMMENT'
 export const EDIT_COMMENT = 'EDIT_COMMENT'
 export const DELETE_COMMENT = 'DELETE_COMMENT'
