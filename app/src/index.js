7ae6a69b05522c40c961f46b41972509651b4d39
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-15 15:39:04 +0900

    Add logger

--- app/src/index.js
+++ app/src/index.js
@@ -5,17 +5,27 @@ import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import thunk from 'redux-thunk'
 
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  composeEnhancers(
+    applyMiddleware(logger, thunk)
+  )
 )
-
-console.log(store.getState())
 
 ReactDOM.render(
   <BrowserRouter>
