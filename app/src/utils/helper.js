f5d603cf2205582c37f889ebada36bb870e3c283
Michi Kanda <michi.kanda.0513@gmail.com>
2017-10-31 22:29:35 +0900

    Add generateID function for creating new posts

--- app/src/utils/helper.js
+++ app/src/utils/helper.js
@@ -2,3 +2,12 @@ export const formatTimestamp = (timestamp) => {
   const date = new Date(timestamp)
   return date.toUTCString()
 }
+
+export function generateID() {
+  function random() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return random()
+}
